52754e00889952de25469f0774e9674c96723aa8
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:05:09 +0300

    Добавление метода для просмотра заказов пользователя

--- src/main/java/myProj/controller/UserController.java
+++ src/main/java/myProj/controller/UserController.java
@@ -1,4 +1,22 @@
 package myProj.controller;
 
+import myProj.localMemory.Const;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+//@RequestMapping(path = "/profi/user")
 public class UserController {
+
+
+    @GetMapping("/profi-user-myorders")
+    protected String viewingOrders(Model model) {
+        model.addAttribute("cities", Const.CITIES);
+        model.addAttribute("states", Const.statesOrder);
+        return "myOrdersUser";
+    }
+
 }
