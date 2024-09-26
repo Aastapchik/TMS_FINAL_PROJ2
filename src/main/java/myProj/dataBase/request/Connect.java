eb2a2a952739767d5c02e8db6c546f1ec67e9dfc
Aastapchik <aastapchik@inbox.ru>
2024-09-26 19:59:59 +0300

    Создание подключения к базе данных

--- src/main/java/myProj/dataBase/request/Connect.java
+++ src/main/java/myProj/dataBase/request/Connect.java
@@ -1,4 +1,24 @@
-package myProj.dataBase.request;
 
+package myProj.dataBase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
 public class Connect {
+
+    @Value("${spring.datasource.url}")
+    private static String URL;
+    @Value("${spring.datasource.username}")
+    private static String USERNAME;
+    @Value("${spring.datasource.password}")
+    private static String PASSWORD;
+
+
+    public static Connection getConnect() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
