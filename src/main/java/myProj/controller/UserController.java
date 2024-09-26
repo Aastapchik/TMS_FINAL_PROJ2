cbc3f73923933e28e33e4dafb7fe5e7483948fbb
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:09:42 +0300

    Добавлена возможность просмотра заказов для USER

--- src/main/java/myProj/controller/UserController.java
+++ src/main/java/myProj/controller/UserController.java
@@ -4,8 +4,8 @@ import myProj.localMemory.Const;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
+import static myProj.dataBase.request.user.UserRequestDB.getUserOrderFromModel;
+import static myProj.dataBase.request.user.UserRequestDB.getUsernameFromModel;
 
 @Controller
 //@RequestMapping(path = "/profi/user")
@@ -16,6 +16,8 @@ public class UserController {
     protected String viewingOrders(Model model) {
         model.addAttribute("cities", Const.CITIES);
         model.addAttribute("states", Const.statesOrder);
+        getUserOrderFromModel(model, 1);
+        getUsernameFromModel(model, 1);
         return "myOrdersUser";
     }
 
