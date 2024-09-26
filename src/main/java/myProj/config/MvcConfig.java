868fc2c9bbddc09ae19df4e3594df0ee3b6597a0
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:06:56 +0300

    Внедрение защиты приложения Spring Security

--- src/main/java/myProj/config/MvcConfig.java
+++ src/main/java/myProj/config/MvcConfig.java
@@ -1,4 +1,14 @@
-package myProj.config;
-
-public class MvcConfig {
-}
+//package myProj.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class MvcConfig implements WebMvcConfigurer {
+//
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/profi-user-login").setViewName("loginForm");
+//    }
+//
+//}
