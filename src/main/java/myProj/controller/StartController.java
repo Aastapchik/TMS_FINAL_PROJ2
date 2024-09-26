511fa30ccb9b21b11d553746d2252676c340ae12
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:05:57 +0300

    Описание методов для перехода гостя по кнопкам на главной странице

--- src/main/java/myProj/controller/StartController.java
+++ src/main/java/myProj/controller/StartController.java
@@ -1,4 +1,35 @@
 package myProj.controller;
 
+import myProj.localMemory.Const;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//@RequestMapping(path = "/profi")
 public class StartController {
+
+    @GetMapping(path = "/profi-welcome")
+    private String welcome(Model model){
+
+        model.addAttribute("cities", Const.CITIES);
+        return "startPagePROFI";
+    }
+
+    @GetMapping(path = "/profi-registration")
+    private String registration(Model model){
+        model.addAttribute("cities", Const.CITIES);
+        return "registrationForm";
+    }
+
+    @GetMapping(path = "/profi-logout")
+    private String logout(Model model){
+        model.addAttribute("cities", Const.CITIES);
+        return "startPagePROFI";
+    }
+    @GetMapping(path = "/profi-welcome-user")
+    private String pageAfterAuth(){
+        return "startPagePROFI";
+    }
+
 }
