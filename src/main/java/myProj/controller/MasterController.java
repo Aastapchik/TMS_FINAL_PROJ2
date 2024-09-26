48a20f1bab0a2d714a4bdb621ce96330db5f7624
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:04:11 +0300

    Добавление метода для вызова формы авторизации для мастера

--- src/main/java/myProj/controller/MasterController.java
+++ src/main/java/myProj/controller/MasterController.java
@@ -1,4 +1,18 @@
 package myProj.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+//@RequestMapping(path = "/profi/master")
 public class MasterController {
+
+    @GetMapping(path = "/profi-master-login")
+    private String loginMaster(){
+
+        return "loginForm";
+    }
+
+
+
 }
