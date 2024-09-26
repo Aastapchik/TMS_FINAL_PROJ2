db90ea039d131538863539627f889333c3e32507
Aastapchik <aastapchik@inbox.ru>
2024-09-26 20:01:15 +0300

    Добавление предполагаемых констант, которые будут использоваться

--- src/main/java/myProj/localMemory/Const.java
+++ src/main/java/myProj/localMemory/Const.java
@@ -1,4 +1,12 @@
 package myProj.localMemory;
 
+import java.util.Arrays;
+import java.util.List;
+
 public interface Const {
+
+    public List<String> CITIES = Arrays.asList("Минск", "Витебск", "Могилев", "Гродно", "Гомель", "Брест");
+
+    public List<String> statesOrder = Arrays.asList("Отменён", "Выполняется", "Ожидание");
+
 }
