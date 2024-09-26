a8f800c98c385c3286b2f209ae099a10977be9bf
Aastapchik <aastapchik@inbox.ru>
2024-09-26 19:59:24 +0300

    Конфигурация entityManagerFactory

--- src/main/java/myProj/dataBase/request/AppCfg.java
+++ src/main/java/myProj/dataBase/request/AppCfg.java
@@ -1,4 +1,25 @@
-package myProj.dataBase.request;
+package myProj.dataBase;
 
+import myProj.entity.*;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppCfg {
+
+    @Bean
+    public static SessionFactory entityManagerFactory() {
+        return new org.hibernate.cfg.Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(AvailableOrder.class)
+                .addAnnotatedClass(SphereActivity.class)
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(UserAOrder.class)
+                .addAnnotatedClass(UserCard.class)
+                .addAnnotatedClass(UserOrder.class)
+                .addAnnotatedClass(UserReview.class)
+                .buildSessionFactory();
+    }
 }
+
+
